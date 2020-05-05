58319126a194f9dc8408d206838cdbcf934cf7a9
8ceg6 <8ceg6@comcast.net>
2020-05-04 23:41:39 -0400

    Done.

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -2,7 +2,7 @@ class TitleValidator < ActiveModel::EachValidator
 
     def validate_each(record,attribute,value)
         unless value =~ /(Won't Believe| Secret| Top [number]| Guess)/
-            record.errors[attribute] << ("Not a clickbait title")
+            record.errors[attribute] << ("Not a clickbait ")
         end
     end 
 end
